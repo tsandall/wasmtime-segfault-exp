0b62cf333d9822ae828acb1bc0ce7cc7e064a438
Torin Sandall <torinsandall@gmail.com>
2021-02-19 16:19:00 -0500

    minimize example

--- main.go
+++ main.go
@@ -15,39 +15,22 @@ func main() {
 
 	bs, err := wasmtime.Wat2Wasm(`
 	(module
-		(import "" "f2" (func $f2))
 		(func (export "f1")
-			(unreachable))
+			(unreachable)))
 	`)
 	check(err)
 
-	for {
-
+	for i := 0; ; i++ {
+		fmt.Println("i:", i)
 		store := wasmtime.NewStore(wasmtime.NewEngine())
-
-		imports := []*wasmtime.Extern{
-			wasmtime.NewFunc(store, wasmtime.NewFuncType(nil, nil), func(c *wasmtime.Caller, args []wasmtime.Val) ([]wasmtime.Val, *wasmtime.Trap) {
-				panic("xxx")
-				return nil, nil
-			}).AsExtern(),
-		}
 
 		module, err := wasmtime.NewModule(store.Engine, bs)
 		check(err)
 
-		instance, err := wasmtime.NewInstance(store, module, imports)
+		instance, err := wasmtime.NewInstance(store, module, nil)
 		check(err)
 
-		err = func() error {
-			defer func() {
-				if r := recover(); r != nil {
-					fmt.Println(r)
-				}
-			}()
-
-			_, err = instance.GetExport("f1").Func().Call()
-			return err
-		}()
+		_, err = instance.GetExport("f1").Func().Call()
 		if err == nil {
 			panic("expected error")
 		}
